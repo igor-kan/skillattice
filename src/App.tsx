cf20bceed1eee3a7649ffe415338aedcb18b6602
Igor Kan <igor.kan.zakhidoff@gmail.com>
2025-06-09 07:57:24 -0400

    Fix GitHub Pages - update workflow path, script paths, HashRouter, remove deploy scripts, empty base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Dashboard from "./components/Dashboard";
 import SkillTree from "./components/SkillTree";
@@ -17,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <div className="min-h-screen bg-gradient-to-br from-slate-50 to-indigo-50">
           <Navigation />
           <Routes>
@@ -31,7 +30,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
